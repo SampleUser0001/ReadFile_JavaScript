84c6f787d1b43b296c090f5ebb2ee3ec9de85864
SampleUser0001 <ittimfn+github@gmail.com>
2022-10-19 13:28:43 +0000

    csvファイル読み込み

--- README.md
+++ README.md
@@ -5,14 +5,30 @@ JavaScriptでファイル読み込みする。
 
 ### text
 
-``` txt
-cd read_csv
+``` bash
+cd read_file
 node app.js ${ファイルパス}
 ```
 
 ### csv
 
+#### 初回
 
+``` bash
+cd read_csv
+npm init -y
+npm install --save csv-perse
+touch read_csv.js
+```
+
+#### 2回目以降
+
+``` bash
+cd read_csv
+npm install
+
+node read_csv.js ${csvファイルパス}
+```
 
 ### json
 
@@ -20,3 +36,4 @@ node app.js ${ファイルパス}
 
 - [node.jsでファイルの入出力操作:Qiita](https://qiita.com/shirokuman/items/509b159bf4b8dd1c41ef)
 - [Node.jsでコマンドライン引数を取得する:Qiita](https://qiita.com/furusin_oriver/items/f030d1eaa9e7b54233c3)
+- [Node.jsでCSVファイルを読み込む方法:Zenn](https://zenn.dev/tatsuyasusukida/articles/nodejs-csv-read)
--- read_csv/read_csv.js
+++ read_csv/read_csv.js
@@ -1,3 +1,37 @@
 'use strict'
 
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
+const {parse} = require("csv-parse/sync");
+
+function main(csvPath) {
+    const source = path.join(__dirname, csvPath);
+    const buffer = fs.readFileSync(source);
+    const options = {escape: '\\'};
+    const {ok, err} = canParse(buffer, options);
+    
+    if(ok) {
+        const rows = parse(buffer, options);
+        console.info(rows);
+    } else {
+        console.error(err);
+    }
+}
+
+function canParse(data, options) {
+    let ok, message;
+    try {
+        parse(data, options);
+        return {ok: true, err: null};
+    } catch(err) {
+        return {ok: false, err};
+    }
+}
+
+if (require.main === module) {
+    if (process.argv.length <= 2) {
+        console.error("ファイル名を指定してください。");
+    } else {
+        main(process.argv[2]);
+    }
+}
